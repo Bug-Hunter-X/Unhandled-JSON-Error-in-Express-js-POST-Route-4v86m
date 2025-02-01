0d33a97e89f76e4023a68022e34dc22f86c71d02
BugHunter-X <askzenai@gmail.com>
2025-02-01 08:38:39 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  // Missing error handling if user is not properly formatted
+  console.log('Creating user:', user);
+  res.status(201).send(user);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
